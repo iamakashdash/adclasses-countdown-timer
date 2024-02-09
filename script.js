bc9820ae0751e9e02711214c75184001a5df389d
Akash Dash <akashdash@Akashs-MacBook-Pro.local>
2024-02-10 04:51:46 +0530

    More subjects added

--- script.js
+++ script.js
@@ -25,4 +25,7 @@ function countdown(date, elementId) {
 countdown("2024-02-27T10:30:00Z", "chemistry-timer");
 countdown("2024-03-04T10:30:00Z", "physics-timer");
 countdown("2024-03-09T10:30:00Z", "maths-timer");
+countdown("2024-03-18T10:30:00Z", "economics-timer");
 countdown("2024-03-19T10:30:00Z", "biology-timer");
+countdown("2024-03-23T10:30:00Z", "accountancy-timer");
+countdown("2024-03-27T10:30:00Z", "business-studies-timer");
